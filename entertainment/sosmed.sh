f48f08f9dbe99083b421472251cd22c5a7212551
ajitirto <ajiwoke@gmail.com>
2024-06-04 21:45:53 +0700

    main:  entertainmen

---
+++ entertainment/sosmed.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+wmctrl -s 3
+
+# Membuka Google Chrome dengan background
+google-chrome &
+
+sleep 1
+
+open_url() {
+  local url=$1
+  xdotool key ctrl+l  # Fokus ke address bar
+  sleep 1
+  xdotool type "$url"  # Ketik URL
+  sleep 1
+  xdotool key KP_Enter  # Tekan Enter
+  sleep 4
+}
+
+
+open_url 'twitter.com'
+
+xdotool key ctrl+t
+sleep 1
+open_url 'facebook.com'
+
+xdotool key ctrl+t
+sleep 1
+open_url 'netflix.com'
+
+xdotool key ctrl+t
+sleep 1
+open_url 'otakudesu.cloud'
+
+
+wmctrl -s 0
+
+echo "task done, thankyou"
